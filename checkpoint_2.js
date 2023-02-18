6df11d4e238aa9a921fef23807a75e05b8f6f100
YoussefAhmed108 <youssef.ahmed108@gmail.com>
2023-02-18 20:57:08 +0200

    first commit

---
+++ checkpoint_2.js
@@ -0,0 +1,11 @@
+let a = [1,4,-5,3,-2,10,-6,20]
+let min = a[0]+a[1]
+for(let i=0;i<a.length;i++){
+    for(let j=i+1;j<a.length;j++){
+        var temp = Math.abs(a[i] + a[j])
+        if(min > temp)
+        min = temp
+    }
+}
+
+console.log(min);
